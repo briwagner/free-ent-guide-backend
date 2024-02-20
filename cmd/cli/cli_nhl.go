c174257fe4b41034560f71ad4982dd369a1d3de7
Brian Wagner <briwagner@outlook.com>
2024-02-20 12:20:59 -0600

    add nhl teams file to embed, so we can run it remotely

--- cmd/cli/cli_nhl.go
+++ cmd/cli/cli_nhl.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
 	"free-ent-guide-backend/models"
 	"free-ent-guide-backend/models/modelstore"
 	"free-ent-guide-backend/pkg/nhlapi"
 	"log"
-	"os"
 	"slices"
 	"time"
 )
@@ -55,17 +55,21 @@ func handleNHL(q *modelstore.Queries, args []string) {
 	}
 }
 
+//go:embed nhl_teams.json
+var nhlTeamData []byte
+
 func seedNHLTeams() error {
 	// These were taken from old api, based on teams from the 2023-24 game schedule.
 	activeIDs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 28, 29, 30, 52, 53, 54, 55, 87, 88, 89, 90, 99}
 
-	data, err := os.ReadFile("pkg/nhlapi/testdata/nhl_teams.json")
-	if err != nil {
-		return err
-	}
+	data := nhlTeamData
+	// data, err := os.ReadFile("pkg/nhlapi/testdata/nhl_teams.json")
+	// if err != nil {
+	// 	return err
+	// }
 
 	var payload nhlapi.GetTeamsPayload
-	err = json.Unmarshal(data, &payload)
+	err := json.Unmarshal(data, &payload)
 	if err != nil {
 		return err
 	}
